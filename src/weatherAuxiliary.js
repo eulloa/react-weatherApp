38607069c1eff56c213bb1728fb989b040341ac0
Efren Ulloa <eulloa345@gmail.com>
2017-06-02 03:18:37 -0500

    adding auxiliary class for helper methods

---
+++ src/weatherAuxiliary.js
@@ -0,0 +1,57 @@
+class WeatherAuxiliary {
+	
+	static getWeatherImage(conditions) {
+		switch(conditions) {
+			case '01d':
+				return 'sunny.png';
+			case '01n':
+				return 'clear.png';
+			case '02d':
+				return 'partly-cloudy.png';
+			case '03d':
+				return 'cloudy.png';
+			case '09d':
+				return 'rain.png';
+			case '10d':
+				return 'drizzle.png';
+			case '11d':
+				return 'thunderstorms.png';
+			case '50d':
+				return 'smoke.png';	
+			default:
+				return 'sunny.png';
+		}
+	}
+	
+	static getDayAndTime() {
+		let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		let d = new Date(),
+		    dayOfWeek = daysOfWeek[d.getDay()],
+		    hours = d.getHours(),
+		    mins  = d.getMinutes(),
+		    amOrPm = 'AM';
+		
+		if (hours > 12) {
+			hours = hours - 12;
+			amOrPm = "PM";
+		} else if (hours === 0) {
+			hours = 12;
+		}
+		
+		if (mins < 10) {
+			mins = "0" + mins;
+		}
+		 
+		return dayOfWeek + " " + hours + ":" + mins + " " + amOrPm;
+	}
+	
+	static kelvinToF(temp) {
+		return parseFloat(9/5 * (temp - 273) + 32).toFixed(2);
+	}
+	
+	static kelvinToC(temp) {
+		return parseFloat(temp - 273).toFixed(2);
+	}
+}
+
+export default WeatherAuxiliary;
--- src/weatherManagerContainer.js
+++ src/weatherManagerContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WeatherDisplayer from './weatherDisplayer';
+import WeatherAuxiliary from './weatherAuxiliary';
 import Input from './input';
 import $ from 'jquery';
 
@@ -30,7 +31,7 @@ class WeatherManagerContainer extends React.Component {
 							onClickTemperature={this.handleClickTemperature}
 							imgSrc={this.state.weatherImage} 
 							city={this.state.city}
-							timeInfo={this.getDayAndTime()}
+							timeInfo={WeatherAuxiliary.getDayAndTime()}
 							tempFahrenheit={this.state.tempFahrenheit}
 							tempCelcius={this.state.tempCelcius}
 							shouldHideF={this.state.shouldHideF}
@@ -77,71 +78,18 @@ class WeatherManagerContainer extends React.Component {
 	downloadWeather(city, apiKey) {
 		return $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + apiKey).then((data) => {			
 			if (data) {
-				console.log(data.main.temp);
+				console.log(data);
 				this.setState({
 					city: data.name,
-					weatherImage: this.getWeatherImage(data.weather[0].icon),
+					weatherImage: WeatherAuxiliary.getWeatherImage(data.weather[0].icon),
 					weather: data,
-					tempFahrenheit: this.kelvinToF(data.main.temp),
-					tempCelcius: this.kelvinToC(data.main.temp)
+					tempFahrenheit: WeatherAuxiliary.kelvinToF(data.main.temp),
+					tempCelcius: WeatherAuxiliary.kelvinToC(data.main.temp)
 				});	
 			} else {
 				console.log('something went wrong!');
 			}
 		});
-	}
-	
-	getDayAndTime() {
-		let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		let d = new Date(),
-		    dayOfWeek = daysOfWeek[d.getDay()],
-		    hours = d.getHours(),
-		    mins  = d.getMinutes(),
-		    amOrPm = 'AM';
-		
-		if (hours > 12) {
-			hours = hours - 12;
-			amOrPm = "PM";
-		} else if (hours === 0) {
-			hours = 12;
-		}
-		
-		if (mins < 10) {
-			mins = "0" + mins;
-		}
-		 
-		return dayOfWeek + " " + hours + ":" + mins + " " + amOrPm;
-	}
-	
-	getWeatherImage(conditions) {
-		switch(conditions) {
-			case '01d':
-				return 'sunny.png';
-			case '01n':
-				return 'clear.png';
-			case '02d':
-				return 'partly-cloudy.png';
-			case '03d':
-				return 'cloudy.png';
-			case '09d':
-				return 'rain.png';
-			case '10d':
-				return 'drizzle.png';
-			case '11d':
-				return 'thunderstorms.png';
-			case '50d':
-				return 'smoke.png';	
-			default:
-				return 'sunny.png';
-		}
-	}
-	
-	kelvinToF(temp) {
-		return parseFloat(9/5 * (temp - 273) + 32).toFixed(2);
-	}
-	
-	kelvinToC(temp) {
-		return parseFloat(temp - 273).toFixed(2);
 	}
 }
 
